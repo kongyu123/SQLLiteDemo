c15f61c0767757ed1db98c3c3b24c478e36a8560
kongyu123 <1759699285@qq.com>
2019-09-10 20:45:09 +0800

    SQLLiteDemo

---
+++ app/src/main/java/com/example/sqllitedemo/activity/activity_insert.java
@@ -0,0 +1,15 @@
+package com.example.sqllitedemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sqllitedemo.R;
+
+public class activity_insert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
